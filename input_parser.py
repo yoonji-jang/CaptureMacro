cf40e6f80cbee3cc831296c4d11b1cb21b26fcf9
yoonji jang <jjyjjjjjjj@gmail.com>
2023-09-17 11:01:14 +0900

    fix to support '=' in url:

--- input_parser.py
+++ input_parser.py
@@ -1,17 +1,20 @@
-import time
-
 from typing import List, Tuple
 
-
 def parse_input_data(input_data: List[str]) -> Tuple[List[str], str, int]:
+    websites = []
     dict_ = {}
-    for line in input_data:
-        key_value = line.strip().split("=")
-        if len(key_value) == 2:
-            dict_[key_value[0]] = key_value[1]
 
-    websites = list(dict_["WEBSITE"].split(","))
-    output_path = dict_["OUTPUT"]
-    capture_interval = dict_["TIME_INTERVAL"]
+    for line in input_data:
+        parts = line.strip().split("=", 1)  # 최대 1번만 나누기
+        if len(parts) == 2:
+            key, value = parts
+            dict_[key] = value
+
+    website_urls = dict_.get("WEBSITE", "").split(",")
+    for url in website_urls:
+        websites.append(url.strip())
+
+    output_path = dict_.get("OUTPUT", "")
+    capture_interval = int(dict_.get("TIME_INTERVAL", 0))
 
     return websites, output_path, capture_interval
