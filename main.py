b9460af243e9d6e88d9d95434635ff637fdc2c97
yoonji.jang <jjyjjjjjjj@gmail.com>
2024-07-10 13:51:21 +0900

    modify to use pyautogui.screenshot

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from driver import *
 import argparse
 from input_parser import parse_input_data
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-from PIL import Image
-from PIL import ImageGrab
 import pyautogui
 import os
 
@@ -46,20 +42,13 @@ while True:
 
         # 웹 브라우저 창을 최대화
         driver.maximize_window()
-
-        # Print Screen 키를 눌러 화면을 캡처하고 클립보드에 저장
-        pyautogui.hotkey("printscreen")
-
-        # 클립보드에서 이미지 가져오기
-        screenshot = ImageGrab.grabclipboard()
-
-        # 캡처된 이미지를 Pillow를 사용하여 저장 (모드를 RGB로 변경)
-        screenshot = screenshot.convert("RGB")
+        time.sleep(5)
 
         # 이미지 파일 이름을 현재 시간으로 생성
         image_filename = "capture_" + time.strftime("%Y%m%d%H%M%S") + ".jpg"
         image_path = os.path.join(current_output_path, image_filename)
-        screenshot.save(image_path)
+        pyautogui.screenshot(image_path) #전체 화면 스크린샷 찍고 파일로 저장
+        # screenshot.save(image_path)
 
     # 웹 드라이버 종료
     driver.quit()
