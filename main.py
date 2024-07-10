3b8b6614fc189e98871cc2b8d83effdf4aff4993
yoonji.jang <jjyjjjjjjj@gmail.com>
2024-07-10 13:52:56 +0900

    version up to 1.3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parser.add_argument('-input', type=str, dest="input_txt", action='store', defaul
 args = parser.parse_args()
 
 # version info
-VERSION = 1.2
+VERSION = 1.3
 print("[Info] CaptureMacro V" + str(VERSION))
 
 # config setting
